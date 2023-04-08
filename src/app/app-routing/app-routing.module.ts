f46086b7f5188ef26c4a655f88f0565240bdbb03
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-08 22:27:46 +0530

    added the JwtRouteGuardService to AppRoutingModule

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import {Routes,RouterModule} from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { HomepageComponent } from '../homepage/homepage.component';
+import { JwtRouteGuardService } from '../jwt-route-guard.service';
 
 const routes:Routes=[
   {path:'login',component:LoginComponent},
-  {path:'home',component:HomepageComponent}
+  {
+    path:'home',
+    component:HomepageComponent,
+    canActivate:[JwtRouteGuardService]
+  }
 ];
 
 @NgModule({
