012d88b457d0b9e9831065820e698fb9a984c04d
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-08 22:12:20 +0530

    stored the server uri in app component at the root but didnt use it anywhere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverURI:string='http://localhost:8000/profile';
   title = 'firstangular';
 }
