6803858cf053e5ae1dce0851772d7a315ae02181
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-09 19:51:55 +0530

    implemented the fetch logic for home page i.e. my blogs

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { TokenstorageService } from '../tokenstorage.service';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
 })
 export class HomepageComponent {
   myblogs:any=[];
-
+  token:any=null;
+  uri:string='http://localhost:8000/blog/home';
+  constructor(private tokenService:TokenstorageService){}
+  async ngOnInit(){
+    this.token=this.tokenService.getToken();
+    const response=await fetch(this.uri,{
+      method:'GET',
+      mode:'cors',
+      credentials:'include',
+      headers:{
+        'Authorization':`Bearer ${this.token}`
+      }
+    });
+    this.myblogs=await response.json();
+  }
   onDelete(blogid:any){
 
   }
