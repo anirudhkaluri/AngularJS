6c48571a38cfe8401a70a4a7dd81a64d5d642975
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-09 20:34:39 +0530

    created data connection between create blog view template and its component class

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-blog',
   templateUrl: './create-blog.component.html',
-  styleUrls: ['./create-blog.component.css']
+
 })
 export class CreateBlogComponent {
 
