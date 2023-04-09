6727b9289d9eaa267145aea64869d607f2712252
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-09 20:46:05 +0530

    used Output() to send the newly added blog to the parent component i.e. homepage

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TokenstorageService } from '../tokenstorage.service';
 
 @Component({
   selector: 'app-create-blog',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
 
 })
 export class CreateBlogComponent {
+  blog_title:string='';
+  blog_content:string='';
+  token:any='';
+  uri:string='http://localhost:8000/blog/add';
+  @Output() dataevent=new EventEmitter();
+  constructor(private tokenService:TokenstorageService){}
+  async onSubmit(){
+    this.token=this.tokenService.getToken();
+    const response=await fetch(this.uri,{
+      mode:'cors',
+      credentials:'include',
+      method:'POST',
+      headers:{
+        'Authorization':`Bearer ${this.token}`,
+        'content-type':'application/json'
+      },
+      body:JSON.stringify({blog_title:this.blog_title,blog_content:this.blog_content})
+    });
+    const blog_added=await response.json();
+    this.dataevent.emit(blog_added);
+  } 
 
+  onTitleChange(eventData:any){
+    this.blog_title=(<HTMLInputElement>eventData.target).value;
+  }
+
+  onContentChange(eventData:any){
+    this.blog_content=(<HTMLInputElement>eventData.target).value;
+  }
 }
